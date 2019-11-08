a052a0a1f67001253e26afb0eedf038bf320e5d4
salmakona <gugly2009@gmail.com>
2019-11-08 20:41:51 +0600

    update calender

--- src/App.js
+++ src/App.js
@@ -3,32 +3,11 @@ import React from 'react';
 import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
 import './styles/style.scss';
 import {  NumberWidget } from 'components/Widget';
+import { FormControl, Grid, Row, Col } from 'react-bootstrap';
+import moment from 'moment';
+import DateTimeRangeContainer from './lib/index';
+import { isFirefoxBelow53 } from './lib/utils/BrowserVersion';
 
-import {
-  // Badge,
-  // Button,
-  // Card,
-  // CardBody,
-  // CardDeck,
-  // CardGroup,
-  // CardHeader,
-  // CardTitle,
-  Col,
-  // ListGroup,
-  // ListGroupItem,
-  Row,
-} from 'reactstrap';
-// import { Bar, Line } from 'react-chartjs-2';
-// import {
-//   MdBubbleChart,
-//   MdInsertChart,
-//   MdPersonPin,
-//   MdPieChart,
-//   MdRateReview,
-//   MdShare,
-//   MdShowChart,
-//   MdThumbUp,
-// } from 'react-icons/md';
 
 const getBasename = () => {
   return `/${process.env.PUBLIC_URL.split('/').pop()}`;
@@ -52,7 +31,89 @@ class App extends React.Component {
     this.state = {
       selectedOption: "option1"
     };
+
+    let start = moment(new Date(2019, 8, 20, 0, 0, 0, 0));
+    let end = moment(start)
+      .add(5, 'days')
+      .subtract(1, 'second');
+    this.state = {
+      start: start,
+      end: end,
+    };
+
+    this.onClick = this.onClick.bind(this);
+    this.applyCallback = this.applyCallback.bind(this);
   }
+
+
+  // datepicker 
+
+  
+local = {
+  "format":"DD-MM-YYYY HH:mm",
+  "sundayFirst" : false
+}
+
+applyCallback(startDate, endDate) {
+  console.log('Apply Callback');
+  console.log(startDate.format('DD-MM-YYYY HH:mm'));
+  console.log(endDate.format('DD-MM-YYYY HH:mm'));
+  this.setState({
+    start: startDate,
+    end: endDate,
+  });
+}
+
+rangeCallback(index, value) {
+  console.log(index, value);
+}
+
+onClick() {
+  let newStart = moment(this.state.start).subtract(3, 'days');
+  // console.log("On Click Callback");
+  // console.log(newStart.format("DD-MM-YYYY HH:mm"));
+  this.setState({ start: newStart });
+}
+
+renderVanillaPicker(ranges, local, maxDate) {
+  let value = `${this.state.start.format('DD-MM-YYYY HH:mm')} - ${this.state.end.format('DD-MM-YYYY HH:mm')}`;
+  let firefoxBelow35 = isFirefoxBelow53();
+  let disabled = true;
+  if (firefoxBelow35) {
+    disabled = false;
+  }
+  return (
+    <div>
+      <div onClick={this.onClick}></div>
+      <div> </div>
+      <br />
+      <DateTimeRangeContainer
+        ranges={ranges}
+        start={this.state.start}
+        end={this.state.end}
+        local={local}
+        maxDate={maxDate}
+        applyCallback={this.applyCallback}
+        rangeCallback={this.rangeCallback}
+        smartMode
+      >
+        <FormControl
+          id="formControlsTextB"
+          type="text"
+          label="Text"
+          placeholder="Enter text"
+          style={{ cursor: 'pointer' }}
+          disabled={disabled}
+          value={value}
+        />
+      </DateTimeRangeContainer>
+      <br />
+    </div>
+  );
+}
+
+  //
+
 
   handleOptionChange = changeEvent => {
     this.setState({
@@ -69,6 +130,29 @@ class App extends React.Component {
   
   render() {
 
+    // 
+    let now = new Date();
+    let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0));
+    let end = moment(start)
+      .add(1, 'days')
+      .subtract(1, 'seconds');
+    let ranges = {
+      'Today Only': [moment(start), moment(end)],
+      'Yesterday Only': [moment(start).subtract(1, 'days'), moment(end).subtract(1, 'days')],
+      '3 Days': [moment(start).subtract(3, 'days'), moment(end)],
+      '5 Days': [moment(start).subtract(5, 'days'), moment(end)],
+      '1 Week': [moment(start).subtract(7, 'days'), moment(end)],
+      '2 Weeks': [moment(start).subtract(14, 'days'), moment(end)],
+      '1 Month': [moment(start).subtract(1, 'months'), moment(end)],
+      '1 Year': [moment(start).subtract(1, 'years'), moment(end)],
+    };
+    let local = {
+      format: 'DD-MM-YYYY HH:mm',
+      sundayFirst: false,
+    };
+    let maxDate = moment(end).add(24, 'hour');
+    //
+
     return (
       <BrowserRouter basename={getBasename()}>
           <Switch>
@@ -81,7 +165,11 @@ class App extends React.Component {
 
                     </div>
                     <div className="mainchlidright">
-                    <button type="button" className="btn btn-default">Calender</button>
+                      <div className="clander"></div>
+                      <div className="container">
+                      {this.renderVanillaPicker(ranges, local, maxDate)}
+
+                    </div>  
                     </div>
                 </div>
 
